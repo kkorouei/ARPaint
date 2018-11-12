c8cfd49a60057e66e89972d692aad6764fc7b0ce
Koushan Korouei <kkorouei@gmail.com>
2018-11-12 11:02:53 +0330

    - Algorithm for finding points on a 3D line added - SCNVector3 Extensions

--- ARPaint/ViewController.swift
+++ ARPaint/ViewController.swift
@@ -25,6 +25,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         let scene = SCNScene()
         sceneView.scene = scene
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(screenTapped))
+        sceneView.addGestureRecognizer(tapGesture)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -40,12 +43,44 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
+    @objc func screenTapped(gesture: UITapGestureRecognizer) {
+        guard let currentPoint = sceneView.pointOfView?.position else { return }
+        if let previousPoint = previousPoint {
+            // Calculate the distance between previous point and current point
+            let distance = previousPoint.distance(vector: currentPoint)
+            print("Distance: \(distance)")
+            let distanceBetweenEachSphere: Float = 0.01
+            let numberOfSpheresToCreate = Int(distance / distanceBetweenEachSphere)
+            print("Number of spheres to create: \(numberOfSpheresToCreate)")
+            // https://math.stackexchange.com/a/83419
+            // Begin by creating a vector BA by subtracting A from B (A = previousPoint, B = currentPoint)
+            let vectorBA = currentPoint - previousPoint
+            print("Vector BA = x:\(vectorBA.x), y:\(vectorBA.y) z:\(vectorBA.z)")
+            // Normalize vector BA by dividng it by it's length
+            let vectorBANormalized = vectorBA.normalized()
+            print("vector normalized = x:\(vectorBANormalized.x), y:\(vectorBANormalized.y), z:\(vectorBANormalized.z)")
+            // This new vector can now be scaled and added to A to find the point at the specified distance
+            for i in 0...numberOfSpheresToCreate {
+                let sphere = SCNSphere(radius: 0.01)
+                sphere.firstMaterial?.diffuse.contents = UIColor.green
+                let sphereNode = SCNNode(geometry: sphere)
+                sphereNode.position = previousPoint + (vectorBANormalized * (Float(i) * distanceBetweenEachSphere))
+                print("Sphere added at position: x:\(sphereNode.position.x), y:\(sphereNode.position.y), z:\(sphereNode.position.z)")
+                sceneView.scene.rootNode.addChildNode(sphereNode)
+            }
+            self.previousPoint = currentPoint
+        } else {
+            self.previousPoint = currentPoint
+        }
+    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         screenTouched = true
         print("Touches began")
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("Touches moved")
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -53,13 +88,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         print("Touches Ended")
     }
     
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        screenTouched = false
+        print("Touches cancelled")
+    }
+    
     func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
         if screenTouched {
             guard let currentTransform = sceneView.session.currentFrame?.camera.transform else { return }
-            // Calculate the distance between previous point and current point
-            // If the distance is greater than x
-            // devide x by constant to get the number of balls to place
-            // place each ball along the line between previous point and current point
             
             let sphere = SCNSphere(radius: 0.01)
             sphere.firstMaterial?.diffuse.contents = UIColor.red
@@ -79,7 +115,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             translation.columns.3.y = 0
             
             //Change The Z Value
-            translation.columns.3.z = -0.2
+            translation.columns.3.z = -0
             
             sphereNode.simdTransform = matrix_multiply(currentTransform, translation)
             scene.rootNode.addChildNode(sphereNode)
