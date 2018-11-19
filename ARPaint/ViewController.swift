bf0ecf7acd77fa268642e519c2bf01ccc21390df
Koushan Korouei <kkorouei@gmail.com>
2018-11-19 11:37:04 +0330

    - sphere is not created if distance hasn't changed much - refactored code

--- ARPaint/ViewController.swift
+++ ARPaint/ViewController.swift
@@ -18,7 +18,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     var previousPoint: SCNVector3?
     var count = 0
     
-    var redBallCount = 0
     var whiteBallCount = 0
     var label: UILabel!
     
@@ -49,6 +48,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
+    // Draws circles between points of the distance between them is greater than x
     func drawCirclesBetween(point1: SCNVector3, andPoint2 point2: SCNVector3){
         // Calculate the distance between previous point and current point
         let distance = point1.distance(vector: point2)
@@ -61,16 +61,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Normalize vector BA by dividng it by it's length
         let vectorBANormalized = vectorBA.normalized()
         // This new vector can now be scaled and added to A to find the point at the specified distance
-        
-        
         for i in 0...((numberOfCirclesToCreate > 1) ? (numberOfCirclesToCreate - 1) : numberOfCirclesToCreate) {
-            let sphere = SCNSphere(radius: 0.01)
-            sphere.firstMaterial?.diffuse.contents = UIColor.red
-            let sphereNode = SCNNode(geometry: sphere)
-            
-            sphereNode.position = point1 + (vectorBANormalized * (Float(i) * distanceBetweenEachCircle))
-            self.sceneView.scene.rootNode.addChildNode(sphereNode)
-            redBallCount += 1
+            let position = point1 + (vectorBANormalized * (Float(i) * distanceBetweenEachCircle))
+            createSphereAndInsert(atPosition: position)
         }
     }
     
@@ -92,36 +85,45 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     // MARK: SCNSceneRendererDelegate methods
     func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
         if screenTouched {
-
-            let sphere = SCNSphere(radius: 0.01)
-            sphere.firstMaterial?.diffuse.contents = UIColor.red
-            let sphereNode = SCNNode(geometry: sphere)
             
-            // Move the node in front of the camera
             guard let cameraTransform = sceneView.session.currentFrame?.camera.transform else { return }
             var translation = matrix_identity_float4x4
             translation.columns.3.x = 0
             translation.columns.3.y = 0
             translation.columns.3.z = -0.2
-            sphereNode.simdTransform = matrix_multiply(cameraTransform, translation)
+            let currentPointTransform = matrix_multiply(cameraTransform, translation)
+            // Convert to SCNVector3
+            let currentPointPosition = SCNVector3Make(currentPointTransform.columns.3.x,
+                                                      currentPointTransform.columns.3.y,
+                                                      currentPointTransform.columns.3.z)
             
-            // Place the sphere in front of the camera
-            scene.rootNode.addChildNode(sphereNode)
-            whiteBallCount += 1
-            
-            let currentPoint = sphereNode.position
             if let previousPoint = previousPoint {
-                let distance = abs(previousPoint.distance(vector: currentPoint))
-                if distance > 0.0055 {
-                    drawCirclesBetween(point1: previousPoint, andPoint2: currentPoint)
+                // Do not create any new spheres if the distance hasn't changed much
+                let distance = abs(previousPoint.distance(vector: currentPointPosition))
+                if distance > 0.0026 {
+                    createSphereAndInsert(atPosition: currentPointPosition)
+                    
+                    drawCirclesBetween(point1: previousPoint, andPoint2: currentPointPosition)
+                    self.previousPoint = currentPointPosition
                 }
+            } else {
+                createSphereAndInsert(atPosition: currentPointPosition)
+                self.previousPoint = currentPointPosition
             }
-            self.previousPoint = currentPoint
             
             DispatchQueue.main.async {
-                self.label.text = "\(self.redBallCount + self.whiteBallCount)"
+                self.label.text = "\(self.whiteBallCount)"
             }
         }
+    }
+    
+    func createSphereAndInsert(atPosition position: SCNVector3) {
+        let sphere = SCNSphere(radius: 0.005)
+        sphere.firstMaterial?.diffuse.contents = UIColor.white
+        let sphereNode = SCNNode(geometry: sphere)
+        sphereNode.position = position
+        sceneView.scene.rootNode.addChildNode(sphereNode)
+        whiteBallCount += 1
     }
     
 }
