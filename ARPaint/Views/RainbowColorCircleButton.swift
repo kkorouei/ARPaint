b04fcf807b7f661242d41ab93a9efae1ad6da186
Koushan Korouei <kkorouei@gmail.com>
2018-12-05 18:08:26 +0330

    - custom circle buttons size adjusted for imageInsets - color buttons sizes fixed

--- ARPaint/Views/RainbowColorCircleButton.swift
+++ ARPaint/Views/RainbowColorCircleButton.swift
@@ -28,10 +28,19 @@ class RainbowColorCircleButton: UIButton {
     
     func setupButton() {
         self.setTitle("", for: .normal)
-        circle = CircleView(frame: self.bounds)
+        
+        // Make sure to adjust for the imageInsets (if they have been changed)
+        let topInset = self.imageEdgeInsets.top
+        let bottomInset = self.imageEdgeInsets.bottom
+        let leftInset = self.imageEdgeInsets.left
+        let rightInset = self.imageEdgeInsets.right
+        
+        circle = CircleView(frame: CGRect(x: 0 + rightInset,
+                                          y: 0 + topInset,
+                                          width: self.bounds.width - (leftInset + rightInset),
+                                          height: self.bounds.height - (topInset + bottomInset)))
         circle.drawRainbowColorCircle()
         addSubview(circle)
-        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
